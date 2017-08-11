f65bc855657ffeb0a32e88c08c2f8a58eee4efe8
Brian Weber <brian@localhost.localdomain>
2017-08-11 00:32:33 -0600

    created files to make website crawler

---
+++ byu-prereq-crawler.js
@@ -0,0 +1,8 @@
+function outputPrereqList() {
+  document.querySelector("textarea").value;
+
+}
+
+function error(errorMessage) {
+  document.querySelector('p#error-output-text').innerHTML += errorMessage;
+}
