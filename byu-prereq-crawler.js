04ab4a6b2ccedfa1075388c537382ba4a7155ffb
Brian Weber <brian.weber1337@gmail.com>
2017-08-23 23:48:51 -0600

    WIP: now parses search results and gets info page for a given class

--- byu-prereq-crawler.js
+++ byu-prereq-crawler.js
@@ -2,6 +2,10 @@ function outputPrereqList() {
   let classList = document.querySelector("textarea#class-list-input").value;
   classList = parseClassList(classList);
   console.log('processed class list: ', classList);
+
+  let classInfoArray = [];
+  for(className of classList){
+  }
 }
 
 function error(errorMessage) {
@@ -22,32 +26,65 @@ function parseClassList(inputString) {
     } else if ( inputStringArray[i].length === 0) {
       inputStringArray.splice(i, 1);
     } else {
-      inputStringArray[i].trim();
-      inputStringArray[i] = inputStringArray[i].replace(/ /g, '+');
+      inputStringArray[i] = inputStringArray[i].trim();
       i++;
     }
   }
   return inputStringArray;
 }
 
-function getClassInfo(){
+function createHtmlDocFromString(htmlDocString){
+  var htmlDoc = document.createElement( 'html' );
+  htmlDoc.innerHTML = htmlDocString;
+  return htmlDoc;
 }
 
-function getClassPage(){
+function parseClassInfoFromClassPage(htmlDocString){
+  console.log('class page we found: ', htmlDocString);
 }
 
-function getClassSearchResultPage(pageUrl){
+function parseClassPageFromSearchResultPage(className, htmlDocString){
+  var htmlDoc = createHtmlDocFromString(htmlDocString);
+  let linksToResults = htmlDoc.querySelector('ol.search-results').querySelectorAll('a');
+  // let searchResults = htmlDoc.querySelectorAll('li.search-result');
+  // let firstSearchResult = htmlDoc.querySelector("li.search-result:first-child");
+  // console.log("search results", searchResults);
+
+  for(foundLink of linksToResults){
+    if(foundLink.innerHTML.match(new RegExp(className, 'i'))){
+      console.log('found it! ', foundLink);
+      console.log('found href: ', foundLink.getAttribute('href'));
+      httpGetWebpageAsyncWithProxy(foundLink.getAttribute('href'), response => {
+        parseClassInfoFromClassPage(response);
+      });
+      return;
+    }
+  }
+  // TODO: make prettier.
+  alert('we couldn\'t find the class \'' + className + '\'. Ensure that it\'s the exact name of the class in the byu catalog and try again!');
 }
 
-function httpGetWebpageAsyncWithProxy(pageUrl){
+function getClassSearchResultPage(className){
+  let classNameForSearch = className.replace(/ /g, '+');
+  httpGetWebpageAsyncWithProxy('https://catalog.byu.edu/search/site/' + classNameForSearch, response => {
+    parseClassPageFromSearchResultPage(className, response);
+  });
+  // inputStringArray[i] = inputStringArray[i].replace(/ /g, '+');
+}
+
+function httpGetWebpageAsyncWithProxy(pageUrl, callback){
+  // TODO: make custom proxy so I don't rely on someone else's
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   var xmlHttp = new XMLHttpRequest();
 
   xmlHttp.onload = () => {
-    console.log('success! ', xmlHttp.responseText);
+    // console.log('success! ', xmlHttp.responseText);
+    callback(xmlHttp.responseText);
   };
   xmlHttp.onerror = () => {
+    // TODO: alert user
     console.log('error: ', xmlHttp);
+    alert('There was a problem retrieving ' + pageUrl + '. Make sure you\'re connected to the internet and that the website we\'re trying to retrieve is available. You can also hit Ctrl-Shift-I (Cmd-Opt-I on mac) and check the console for more info.');
   };
   xmlHttp.open("GET", proxyUrl + pageUrl, true); // true for asynchronous 
   xmlHttp.send(null);
